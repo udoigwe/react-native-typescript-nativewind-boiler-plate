72220ce00277204b53b125ba1e7234681ce8f26a
Udo Igwe Uchechukwu <udoigweuchechukwu@gmail.com>
2025-06-10 09:22:51 +0100

    chore: constants, defaultformdata, icons and images

---
+++ constants/defaultFormData.ts
@@ -0,0 +1,7 @@
+import { ISignInDTO } from "@/interfaces/dtos/auth.dto.interface";
+
+/********************** SIGNIN DEFAULT FORMDATA **********************************/
+export const SIGN_IN_DEFAULT_FORM_DATA: ISignInDTO = {
+	username: "",
+	password: "",
+};
---
+++ constants/images.ts
@@ -0,0 +1,7 @@
+import HIGH_TENSION from "@/assets/images/high-tension.png";
+import LOGO from "@/assets/images/logo.png";
+
+export const images = {
+	LOGO,
+	HIGH_TENSION,
+};
