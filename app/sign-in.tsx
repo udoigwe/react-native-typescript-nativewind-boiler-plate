f9a74ec252312380288fd965d901939b84e9f44d
Udo Igwe Uchechukwu <udoigweuchechukwu@gmail.com>
2025-06-10 23:23:16 +0100

    chore: used useRemember me hook and minor code changes

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -7,6 +7,7 @@ import { images } from "@/constants/images";
 import { useAlert } from "@/context/AlertContext";
 import { useLoading } from "@/context/LoadingContext";
 import { useSignIn } from "@/hooks/queries/auth";
+import { useRememberMe } from "@/hooks/useRememberMe";
 import { ISignInDTO } from "@/interfaces/dtos/auth.dto.interface";
 import { setToken } from "@/utils/auth";
 import { Link, useRouter } from "expo-router";
@@ -26,7 +27,6 @@ const SignInScreen = () => {
 	const { showLoading, hideLoading } = useLoading();
 	const { showAlert } = useAlert();
 	const router = useRouter();
-	const [isChecked, setIsChecked] = useState(false);
 	const [formData, setFormData] = useState<ISignInDTO>(
 		SIGN_IN_DEFAULT_FORM_DATA
 	);
@@ -45,7 +45,12 @@ const SignInScreen = () => {
 			]);
 		},
 	});
-	const onSubmit = () => {
+
+	const { rememberMe, setRememberMe, handleRememberMeStorage } =
+		useRememberMe({ formData, setFormData });
+
+	const onSubmit = async () => {
+		await handleRememberMeStorage(formData.username, formData.password);
 		loginMutation({ payload: formData });
 	};
 
@@ -58,8 +63,15 @@ const SignInScreen = () => {
 	return (
 		<SafeAreaView className="h-full">
 			<StatusBar className="bg-primary-100 text-white" />
-			<ImageBackground source={images.HIGH_TENSION} resizeMode="cover">
-				<ScrollView contentContainerStyle={{ height: "100%" }}>
+			<ImageBackground
+				source={images.HIGH_TENSION}
+				resizeMode="cover"
+				className="flex-grow"
+			>
+				<ScrollView
+					contentContainerStyle={{ flexGrow: 1 }}
+					keyboardShouldPersistTaps="handled"
+				>
 					<KeyboardWrapper className="flex-1 w-full h-full justify-between items-center flex-col px-3 py-11">
 						<Image source={images.LOGO} />
 						<View className="gap-8 py-6 px-4 rounded-[12px] border border-[#FFD26040] bg-[#FFFFFF14] w-full">
@@ -84,7 +96,7 @@ const SignInScreen = () => {
 												username: text,
 											});
 										}}
-										className="h-11 w-full rounded-[8px] border border-[#F0F0F0] py-2 px-3 bg-white text-sm"
+										className="font-poppins h-11 w-full rounded-[8px] border border-[#F0F0F0] py-2 px-3 bg-white text-sm"
 										placeholder="xxxxx.xxxx"
 										placeholderClassName="font-poppins text-sm text-[#999999]"
 									/>
@@ -94,6 +106,7 @@ const SignInScreen = () => {
 										Password
 									</Text>
 									<PasswordInput
+										inputStyles="font-poppins"
 										value={formData.password}
 										onChangeText={(text) => {
 											setFormData({
@@ -106,8 +119,8 @@ const SignInScreen = () => {
 							</View>
 							<View className="flex-row justify-between items-center gap-6">
 								<CustomCheckbox
-									checked={isChecked}
-									onChange={setIsChecked}
+									checked={rememberMe}
+									onChange={setRememberMe}
 									checkedBackgroundColor="bg-[#1E83F0]"
 									uncheckedBackgroundColor="bg-white"
 									label="Remember Me"
